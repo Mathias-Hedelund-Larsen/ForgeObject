8e4a6c6f4ac71b81eeffb941ae1b97f2ce536ed0
MathDodo <mathias.h.larsen@jp.dk>
2020-03-26 15:54:52 +0100

    First implementation of the ForgeObject

---
+++ Assets/HephaestusForge/ForgeObject/ForgeObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public abstract class ForgeObject 
+{
+    /// <summary>
+    /// Get the data of this class as json string
+    /// </summary>
+    /// <returns>A json representation of the class.</returns>
+    public override string ToString()
+    {
+        return ToJsonString();
+    }
+
+    /// <summary>
+    /// Get the data of this class as json string
+    /// </summary>
+    /// <returns>A json representation of the class.</returns>
+    public string ToJsonString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
